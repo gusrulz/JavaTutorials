2d8f695325251a24729c9e40a3b200d75a9e29ed
Thomas Martin <Thomas Martin>
2016-03-25 19:09:05 +1100

    first commit

--- 02 - Helicopter Battle/src/helicopterbattle/Canvas.java
+++ 02 - Helicopter Battle/src/helicopterbattle/Canvas.java
@@ -47,4 +47,90 @@ public abstract class Canvas extends JPanel implements KeyListener, MouseListene
 	
 	// This method is overridden in Framework.java and is used for drawing to the screen.
 	public abstract void Draw(Graphics2D g2d);
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		
+		Graphics2D g2d = (Graphics2D)g;
+		super.paintComponent(g2d);
+		Draw(g2d);
+	}
+	
+	// Keyboard
+	/**
+	 * Is keyboard key "key" down?
+	 * 
+	 * @param key Number of key for which you want to check the state.
+	 * @return true if the key is down, false if the key is not down.	 
+	 */
+	public static boolean keyboardKeyState(int key) {
+		
+		return keyboardState[key];
+	}
+	
+	// Methods of the keyboard listener.
+	@Override
+	public void keyPressed(KeyEvent e) {
+		
+		keyboardState[e.getKeyCode()] = false;
+		keyReleasedFramework(e);
+	}
+	
+	@Override
+	public void keyTyped(KeyEvent e) { }
+	
+	public abstract void keyReleasedFramework(KeyEvent e);
+	
+	// Mouse
+	/**
+	 * Is mouse button "button" down?
+	 * Parameter "button" can be "MouseEvent.BUTTON1" - Indicates mouse button #1
+	 * or "MouseEvent.BUTTON2" - Indicates mouse button #2 ...
+	 * 
+	 * @param button Number of mouse button for which you want to check the state.
+	 * @return true if the button is down, false if the button is not down.
+	 */
+	public static boolean mouseButtonState(int button) {
+		
+		return mouseState[button - 1];
+	}
+	
+	// Sets mouse key status
+	private void mouseKeyStatus(MouseEvent e, boolean status) {
+		
+		if(e.getButton() == MouseEvent.BUTTON1) {
+			
+			mouseState[0] = status;
+		}
+		else if(e.getButton() == MouseEvent.BUTTON2) {
+			
+			mouseState[1] = status;
+		}
+		else if(e.getButton() == MouseEvent.BUTTON3) {
+			
+			mouseState[2] = status;
+		}
+	}
+	
+	// Methods of the mouse listener.
+	@Override
+	public void mousePressed(MouseEvent e) {
+		
+		mouseKeyStatus(e, true);
+	}
+	
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		
+		mouseKeyStatus(e, false);
+	}
+	
+	@Override
+	public void mouseClicked(MouseEvent e) { }
+	
+	@Override
+	public void mouseEntered(MouseEvent e) { }
+	
+	@Override
+	public void mouseExited(MouseEvent e) { }
 }
